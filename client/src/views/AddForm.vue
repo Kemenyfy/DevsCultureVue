4a612be0e71def285b84ecc5b642586638b863de
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-10-11 20:11:54 -0400

    Added Content

--- client/src/views/AddForm.vue
+++ client/src/views/AddForm.vue
@@ -35,6 +35,24 @@
             </div>
             <button class="btn submit" type="submit" value="submit" v-on:click.prevent="submitResource">Submit</button>
           </form>
+          <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Message from Extracurricular</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+          <div class="modal-body">
+              Team Added
+          </div>
+             <div class="modal-footer">
+              <button type="button"  class="btn btn-secondary" data-dismiss="modal">Close</button>
+             </div>
+            </div>
+          </div>
+        </div>
         </div>
     </div>
   </div>
